e7d15227e164be0fe36af445ef5bf61311276106
Zain Ali <chzainali6363@gmail.com>
2022-12-30 11:13:43 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="container">
+    <h1>Baby Name Generator</h1>
+    <p>Choose your option by just click the buttons below!</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1) Choose a gender</h4>
+        <div class="option-buttons">
+          <button
+            class="option option-left"
+            :class="options.gender === Gender.BOY && 'option-active'"
+            @click="(options.gender = Gender.BOY)"
+            >
+            Boy
+          </button>
+          <button
+          class="option"
+          :class="options.gender === Gender.UNISEX && 'option-active'"
+          @click="(options.gender = Gender.UNISEX)"
+          >
+          Unisex
+        </button>
+        <button
+        class="option option-right"
+        :class="options.gender === Gender.GIRL && 'option-active'"
+        @click="(options.gender = Gender.GIRL)"
+          >
+            Girl
+          </button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Choose the name's popularity</h4>
+        <div class="option-buttons">
+          <button
+            class="option option-left"
+            :class="options.popularity === Popularity.TRENDY && 'option-active'"
+            @click="(options.popularity = Popularity.TRENDY)"
+            >
+            Trendy
+          </button>
+          <button
+            class="option option-right"
+            :class="options.popularity === Popularity.UNIQUE && 'option-active'"
+            @click="(options.popularity = Popularity.UNIQUE)"
+          >
+            Unique
+          </button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>3) Choose the name's length</h4>
+        <div class="option-buttons">
+          <button
+            class="option option-left"
+            :class="options.length === Length.LONG && 'option-active'"
+            @click="(options.length = Length.LONG)"
+            >
+            Long
+          </button>
+          <button
+          class="option"
+          :class="options.length === Length.ALL && 'option-active'"
+          @click="(options.length = Length.ALL)"
+          >
+          All
+        </button>
+        <button
+        class="option option-right"
+        :class="options.length === Length.SHORT && 'option-active'"
+        @click="(options.length = Length.SHORT)"
+          >
+            Short
+          </button>
+        </div>
+      </div>
+      <button class="primary" @click="computeSelectedNames">Find Names</button>
+    </div>
+    <div class="cards-container">
+      <div v-for="name in seletedNames" :key="name" class="card">
+        <h4>{{ name }}</h4>
+        <p></p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Gender, Popularity, Length, names } from '@/data';
+
+interface OptionState {
+  gender: Gender;
+  popularity: Popularity;
+  length: Length;
+}
+
+const options = reactive<OptionState>({
+  gender: Gender.GIRL,
+  popularity: Popularity.UNIQUE,
+  length: Length.SHORT,
+});
+
+const computeSelectedNames = () =>{
+  const filteredNames = names.filter((name) => name.gender === options.gender)
+  .filter((name) => name.popularity === options.popularity)
+  .filter((name) => {
+    if (options.length === Length.ALL) return true
+    else return name.length === options.length
+  })
+
+  seletedNames.value = filteredNames.map((name) => name.name)
+}
+
+const seletedNames = ref<string[]>([])
+</script>
+
+<style scoped>
+.container {
+  font-family: Arial, Helvetica, sans-serif;
+  color: rgb(27, 60, 138);
+  max-width: 50rem;
+  margin: 0 auto;
+  text-align: center;
+}
+.container h1 {
+  font-size: 3rem;
+}
+.options-container {
+  background-color: rgb(255, 238, 236);
+  border-radius: 2rem;
+  width: 95%;
+  padding: 1rem;
+  margin: 0 auto;
+  margin-top: 4rem;
+  position: relative;
+}
+.option-container {
+  margin-bottom: 2rem;
+}
+.option {
+  background: white;
+  outline: 0.15rem solid rgb(249, 87, 89);
+  border: none;
+  padding: 0.75rem;
+  width: 12rem;
+  font-size: 1rem;
+  color: rgb(27, 60, 128);
+  cursor: pointer;
+  font-weight: 200;
+}
+.option-left {
+  border-radius: 1rem 0 0 1rem;
+}
+.option-right {
+  border-radius: 0 1rem 1rem 0;
+}
+.option-active {
+  background-color: rgb(249, 87, 89);
+  color: white;
+}
+.primary {
+  background-color: rgb(249, 87, 89);
+  color: white;
+  border: none;
+  border-radius: 30px;
+  padding: 0.75rem 4rem;
+  font-size: 1rem;
+  margin-top: 1rem;
+  cursor: pointer;
+}
+.cards-container {
+  display: flex;
+  margin-top: 3rem;
+  flex-wrap: wrap;
+}
+.card {
+  background-color: rgb(27, 60, 138);
+  width: 28%;
+  color: white;
+  padding: 0.1rem;
+  margin-right: 0.5rem;
+  border-radius: 1rem;
+  margin-bottom: 1rem;
+  position: relative;
+}
+.card p {
+  position: absolute;
+  top: -29%;
+  left: 92.5%;
+  cursor: pointer;
+  color: rgb(255, 255, 255, 0.178);
+}
+</style>
